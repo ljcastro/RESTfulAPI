3dd23c0a0d7391f4c1e24989bab7abe5d96e069e
Lucas Castro <lcastro@jetmultimedia.es>
2015-03-03 17:00:19 +0100

    Creación de seeders
    - Creadas las clases para alimentar la bbdd por medio de seeders y faker

---
+++ database/seeds/VehiculoSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Fabricante;
+use App\Vehiculo;
+use Faker\Factory as Faker;
+
+class VehiculoSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$faker = Faker::create();
+		$cantidad = Fabricante::all()->count();
+
+		for($i = 0; $i < 35; $i++)
+		{
+			Vehiculo::create
+			([
+				'color' => $faker->safeColorName(),
+				'cilindrada' => $faker->randomFloat(),
+				'potencia' => $faker->randomNumber(),
+				'peso' => $faker->randomFloat(),
+				'fabricante_id' => $faker->numberBetween(1,$cantidad)
+			]);
+		}
+	}
+
+}
