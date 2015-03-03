7c7a2f7ef1e9e0fdfe84eebdf6b05f2f9c7ec570
Lucas Castro <lcastro@jetmultimedia.es>
2015-03-03 16:19:33 +0100

    Creación de migraciones
    - Se crean las migraciones para cada modelo
    - Se agrega la columna de clave foránea al modelo Vehículo

--- app/Vehiculo.php
+++ app/Vehiculo.php
@@ -6,7 +6,7 @@ class Vehiculo extends Model
 {
 	protected $table = 'vehiculos';
 	protected $primaryKey = 'serie';
-	protected $fillable = array('color','cilindrada','potencia','peso');
+	protected $fillable = array('color','cilindrada','potencia','peso','fabricante_id');
 
 	public function fabricantes()
 	{
